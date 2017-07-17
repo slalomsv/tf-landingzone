06ae6a90d8fb03902b776899a904787a028699a8
nfollett-slalom <nick.follett@slalom.com>
2017-07-17 10:12:16 -0700

    Split route from route table stanza to allow better modularity

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -73,10 +73,12 @@ resource "aws_eip" "eip" {
 
 resource "aws_route_table" "public_route" {
   vpc_id         = "${aws_vpc.main.id}"
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.public_gateway.id}"
-  }
+}
+
+resource "aws_route"  "public_route" {
+  route_table_id             = "${aws_route_table.public_route.id}"
+  destination_cidr_block     = "0.0.0.0/0"
+  gateway_id                 = "${aws_internet_gateway.public_gateway.id}"
 }
 
 resource "aws_route_table_association" "public" {
