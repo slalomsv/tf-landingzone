cbe69e165db76addcce55ad7dffb0194a427cf77
nfollett-slalom <nick.follett@slalom.com>
2017-07-18 10:27:22 -0700

    Updated to new subnets

---
+++ environments/stage/vpc/standard-vpc/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "${var.aws_region}"
+}
+
+module "stage_open_vpc" {
+  source           = "../../../../modules/vpc"
+  vpc_name         = "${var.vpc_name}"
+  vpc_cidr         = "${var.vpc_cidr}"
+  dmz_subnet_cidr  = "${var.dmz_subnet_cidr}"
+  app_subnet_cidr  = "${var.app_subnet_cidr}"
+  data_subnet_cidr = "${var.data_subnet_cidr}"
+}
+
+module "security_group_public" {
+  source = "../../../../modules/security-group/public"
+  vpc_id = "${module.stage_open_vpc.vpc_id}"
+}
+
