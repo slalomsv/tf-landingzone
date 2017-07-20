f815af878f2b2be0ab63752035c2a9627618f989
nfollett-slalom <nick.follett@slalom.com>
2017-07-20 09:32:06 -0700

    Moved live infra to vpc-peering.tf

--- live/vpc-peering.tf
+++ live/vpc-peering.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-
-###################
-### Management ####
-###################
-module "management_vpc" {
-  source = "../env-modules/management"
-}
-
 module "management_to_prod_standard_peering" {
   source              = "../infra-modules/vpc-peering"
   route_table_id      = "${module.management_vpc.public_route_table_id}"
@@ -60,29 +52,5 @@ module "management_to_stage_pci_peering" {
   peer_name           = "${module.stage_pci_vpc.vpc_name}"
   peer_cidr           = "${module.stage_pci_vpc.vpc_cidr}"
   bidirectional       = true
-}
-
-
-##################
-### Production ###
-##################
-module "prod_standard_vpc" {
-  source = "../env-modules/production/standard-vpc"
-}
-
-module "prod_pci_vpc" {
-  source = "../env-modules/production/pci-vpc"
-}
-
-
-###############
-### Staging ###
-###############
-module "stage_standard_vpc" {
-  source = "../env-modules/stage/standard-vpc"
-}
-
-module "stage_pci_vpc" {
-  source = "../env-modules/stage/pci-vpc"
 }
 
