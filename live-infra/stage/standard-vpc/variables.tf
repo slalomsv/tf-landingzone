dadef7e09597261e9b766b50a00cf7677d059bdc
Nick Follett <nick.follett@slalom.com>
2017-08-04 09:16:39 -0700

    VPC CIDR block fix (#4)
    * Fix VPC CIDR blocks to remove conflicts

--- live-infra/stage/standard-vpc/variables.tf
+++ live-infra/stage/standard-vpc/variables.tf
@@ -10,37 +10,37 @@ variable "vpc_name" {
 
 variable "vpc_cidr" {
   description = "CIDR for the VPC"
-  default     = "10.2.0.0/16"
+  default     = "10.3.0.0/16"
 }
 
 variable "dmz_subnet_1_cidr" {
   description = "CIDR for the DMZ Subnet"
-  default     = "10.2.1.0/24"
+  default     = "10.3.1.0/24"
 }
 
 variable "dmz_subnet_2_cidr" {
   description = "CIDR for the DMZ Subnet"
-  default     = "10.2.2.0/24"
+  default     = "10.3.2.0/24"
 }
 
 variable "app_subnet_1_cidr" {
   description = "CIDR for the Application Subnet"
-  default     = "10.2.3.0/24"
+  default     = "10.3.3.0/24"
 }
 
 variable "app_subnet_2_cidr" {
   description = "CIDR for the Application Subnet"
-  default     = "10.2.4.0/24"
+  default     = "10.3.4.0/24"
 }
 
 variable "data_subnet_1_cidr" {
   description = "CIDR for the Data Subnet"
-  default     = "10.2.5.0/24"
+  default     = "10.3.5.0/24"
 }
 
 variable "data_subnet_2_cidr" {
   description = "CIDR for the Data Subnet"
-  default     = "10.2.6.0/24"
+  default     = "10.3.6.0/24"
 }
 
 variable "example_ws_asg_name" {
